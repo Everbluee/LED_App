01bd1ba99b5d653ee08a4905ad0a6040773e5f9a
katarzyna-pio <katarzynamariapiorkowska@gmail.com>
2024-05-26 00:32:34 +0200

    Colours Activity update

--- app/src/main/java/com/example/led/ColoursActivity.kt
+++ app/src/main/java/com/example/led/ColoursActivity.kt
@@ -1,10 +1,17 @@
 package com.example.led
 
+import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,10 +20,17 @@ import androidx.core.view.WindowInsetsCompat
 class ColoursActivity : AppCompatActivity() {
     private var bluetoothStateReceiver: BluetoothBR = BluetoothBR()
 
-    private lateinit var backPress: Button
+    private lateinit var backPress: ImageButton
+    private lateinit var colorWheel: ImageView
+    private lateinit var colorIndicator: ImageView
+    private lateinit var selectedColorTextView: TextView
+    private lateinit var colorDisplay: View
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_colours)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -27,11 +41,66 @@ class ColoursActivity : AppCompatActivity() {
         LogStorage.logs.add("Przejście do ekranu ( $this )")
 
         //Obsługa przycisku colorButton (przeniesienie do aktywności ColoursActivity)
-        backPress = findViewById(R.id.backPress)
+        colorWheel = findViewById(R.id.colorWheel)
+        colorIndicator = findViewById(R.id.colorIndicator)
+        selectedColorTextView = findViewById(R.id.selectedColorTextView)
+        colorDisplay = findViewById(R.id.colorDisplay)
+        backPress = findViewById(R.id.backButtonAnim)
+        val redButton: Button = findViewById(R.id.redButton)
+        val greenButton: Button = findViewById(R.id.greenButton)
+        val blueButton: Button = findViewById(R.id.blueButton)
+
+        colorWheel.setOnTouchListener { v, event ->
+            if (event.action == MotionEvent.ACTION_DOWN || event.action == MotionEvent.ACTION_MOVE) {
+
+                val imageViewCoordinates = IntArray(2)
+                colorWheel.getLocationOnScreen(imageViewCoordinates)
+                val x = event.rawX - imageViewCoordinates[0]
+                val y = event.rawY - imageViewCoordinates[1]
+
+                if (x >= 0 && y >= 0 && x < colorWheel.width && y < colorWheel.height) {
+                    val bitmap = (colorWheel.drawable as BitmapDrawable).bitmap
+                    val scaledX = (x * (bitmap.width.toFloat() / colorWheel.width)).toInt()
+                    val scaledY = (y * (bitmap.height.toFloat() / colorWheel.height)).toInt()
+
+                    try {
+                        val pixel = bitmap.getPixel(scaledX, scaledY)
+                        val colorString = String.format("#%06X", 0xFFFFFF and pixel)
+                        selectedColorTextView.text = colorString
+                        colorDisplay.setBackgroundColor(pixel)
+                        colorIndicator.x = x - colorIndicator.width / 2
+                        colorIndicator.y = y - colorIndicator.height / 2
+                    } catch (e: IllegalArgumentException) {
+
+                    }
+                }
+            }
+            true
+        }
+
+        redButton.setOnClickListener {
+            val color = 0xFFFF0000.toInt()
+            updateSelectedColor(color)
+        }
+
+        greenButton.setOnClickListener {
+            val color = 0xFF00FF00.toInt()
+            updateSelectedColor(color)
+        }
+        blueButton.setOnClickListener {
+            val color =  0xFF0000FF.toInt()
+            updateSelectedColor(color)
+        }
         backPress.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun updateSelectedColor(color: Int) {
+        val colorString = String.format("#%06X", 0xFFFFFF and color)
+        selectedColorTextView.text = colorString
+        colorDisplay.setBackgroundColor(color)
     }
 
     override fun onResume() {
